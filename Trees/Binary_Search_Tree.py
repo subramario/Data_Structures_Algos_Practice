21581d83adf6007231ab232722cdabc1fd25f44e
a7subram <a7subram@uwaterloo.ca>
2020-05-07 13:39:56 -0400

    Added more methods

--- Trees/Binary_Search_Tree.py
+++ Trees/Binary_Search_Tree.py
@@ -8,6 +8,60 @@ class BST:
     def __init__(self, root):
         self.root = Node(root)
 
+    def get_min(self,start):
+        if start.left is None:
+            print(start.value)
+            return
+        else:
+            self.get_min(start.left)
+
+    def get_max(self,start):
+        if start.right is None:
+            print(start.value)
+            return
+        else:
+            self.get_max(start.right)
+
+    def size(self, start):
+        if start is None:
+            return 0
+
+        left_branch = self.size(start.left)
+        right_branch = self.size(start.right)
+        return (1 + left_branch + right_branch)
+
+    def height(self, start):
+        if start is None:
+            return -1
+
+        left_height = self.height(start.left)
+        right_height = self.height(start.right)
+        return 1 + max(left_height,right_height)
+
+    def print_ascending(self, start):
+        self.inorder_traversal(start)
+
+    def preorder_traversal(self,start):
+        if start:
+            print(start.value)
+            self.preorder_traversal(start.left)
+            self.preorder_traversal(start.right)
+            return
+
+    def inorder_traversal(self, start):
+        if start:
+            self.inorder_traversal(start.left)
+            print(start.value)
+            self.inorder_traversal(start.right)
+            return
+
+    def postorder_traversal(self, start):
+        if start:
+            self.postorder_traversal(start.left)
+            self.postorder_traversal(start.right)
+            print(start.value)
+            return
+
     def insert(self, value, start):
         if value < start.value:
             if start.left is None:
@@ -16,23 +70,30 @@ class BST:
             else:
                 self.insert(value, start.left)
 
-        if value > start.value:
+        elif value > start.value:
             if start.right is None:
                 start.right = Node(value)
                 return
             else:
                 self.insert(value, start.right)
-
-        if value == start:
+        else:
             print("Value already added to binary search tree.")
             return
 
-    def inorder_traversal(self, start):
-        if start:
-            print(start.value)
-            self.inorder_traversal(start.left)
-            self.inorder_traversal(start.right)
-            return
+    def find(self, value, start):
+        if start is None:
+            print(str(value) + " is not in this tree.")
+            return False
+
+        if start.value == value:
+            print(str(value) + " is in this tree.")
+            return True
+        
+        if value > start.value:
+            self.find(value, start.right)
+
+        if value < start.value:
+            self.find(value, start.left)
 
 
 tree = BST(8)
@@ -43,5 +104,4 @@ tree.insert(6, tree.root)
 tree.insert(9, tree.root)
 tree.insert(11, tree.root)
 
-
-tree.inorder_traversal(tree.root)
+print(tree.height(tree.root))
