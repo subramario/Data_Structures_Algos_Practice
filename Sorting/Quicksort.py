913ca932f32276d86c2c7f6c32e4f466121fc1e8
a7subram <a7subram@uwaterloo.ca>
2020-05-17 22:13:28 -0400

    Commented

--- Sorting/Quicksort.py
+++ Sorting/Quicksort.py
@@ -1,3 +1,4 @@
+#Classic quicksort - chooses the first element in the array as the pivot point and sorts accordingly 
 class Solution:
     def partition(self, array, start, end):
         pivot = array[start]
@@ -19,14 +20,14 @@ class Solution:
         array[start], array[high] = array[high], array[start]
 
         return high
-
+    
     def quicksort(self,array,start,end): 
         if start >= end:
             return
         
-        p = self.partition(array, start, end)
-        self.quicksort(array, start, p-1)
-        self.quicksort(array, p+1, end)
+        p = self.partition(array, start, end) #Retrieves the index of the pivot point after each sort
+        self.quicksort(array, start, p-1) #Recursively sorts array on left side of the pivot
+        self.quicksort(array, p+1, end) #Recursively sorts array on right side of the pivot
 
 
 
