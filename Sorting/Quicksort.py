e47c6fd25867b54f1225f3fc982bdfac6ca39c4c
a7subram <a7subram@uwaterloo.ca>
2020-05-18 11:39:09 -0400

    Commented

--- Sorting/Quicksort.py
+++ Sorting/Quicksort.py
@@ -1,25 +1,29 @@
 #Classic quicksort - chooses the first element in the array as the pivot point and sorts accordingly 
 class Solution:
     def partition(self, array, start, end):
+        # [start|low_ _ _ _ _ _high] --> Three pointers 
         pivot = array[start]
         low = start + 1
         high = end 
 
         while True:
-            while low <= high and array[low] <= pivot:
+            # Continue iterating high and low pointers toward each other until an unsorted value is found or if pointers cross each other
+            while low <= high and array[low] <= pivot: 
                 low = low + 1
 
             while low <= high and array[high] >= pivot:
                 high = high - 1
 
+            # If both pointers have unsorted values, swap them into correct position, else if they have crossed each other, break
             if low <= high:
                 array[low], array[high] = array[high], array[low]
             else:
                 break
-
+        
+        # Sort the pivot value into its respective place by switching with high/low pointer
         array[start], array[high] = array[high], array[start]
 
-        return high
+        return high # Note: values are swapped, but pointer locations are the same! Return the location of the now sorted pviot
     
     def quicksort(self,array,start,end): 
         if start >= end:
